28ab13938b46ed7728ae9f6bb8d05fb94c3fea97
ibadsiddiqui <ibadsiddiqui01@outlook.com>
2019-08-13 01:31:28 +0500

    --Commit: linkedlist class created and attributes intiailized

--- 02-LinkedList/linkedlist.py
+++ 02-LinkedList/linkedlist.py
@@ -2,3 +2,10 @@ class Node(object):
     def __init__(self, data):
         super(Node, self).__init__(data)
         self.data = data
+
+
+class LinkedList(object):
+    def __init__(self, ):
+        super(LinkedList, self).__init__()
+        self.headd = None
+        self.size = 0
