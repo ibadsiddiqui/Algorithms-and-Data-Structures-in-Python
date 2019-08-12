130ba676f4d500b2109602212faa93d9cf34613e
ibadsiddiqui <ibadsiddiqui01@outlook.com>
2019-08-13 01:18:42 +0500

    --Commit: indexing and search added

--- 01-Arrays/array.py
+++ 01-Arrays/array.py
@@ -1,4 +1,21 @@
-numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+numbers = [1, 2, 3, 4, 5, 10, 7, 8, 9]
 
 # random indexing -> O(1) get the items we know the index
+# print(numbers[1])
 
+# numbers[1] = "Adams"
+
+# for item in numbers:
+    # print(item)
+
+# for i in range(len(numbers)):
+    # print(numbers[i])
+
+# print(numbers[:2])
+
+# O(N) search running time
+maximum_number = numbers[0]
+for item in numbers:
+    if maximum_number < item:
+        maximum_number = item
+print(maximum_number)
